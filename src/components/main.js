b78daec340069bb68ae01d45fed3311907309968
Shaheer Khawaja <khawajas1@10.34.13.138>
2019-07-20 21:44:49 -0400

    “Styling”

--- src/components/main.js
+++ src/components/main.js
@@ -1,16 +1,20 @@
 import React from 'react';
 import Konva_Wrapper from './konva_wrapper'
 import Info from './info.js'
+{/*import './reset.css';
+import './style.css';*/}
+
 
 
 export default class Main extends React.Component {
+    
     constructor(props) {
         super(props);
         this.state = {
             area_length: '',
             processing: false,
             area_height: '',
-            rectangles: [{length: 0, height: 0}],
+            rectangles: [{length: "", height: ""}],
             points: [],
             values : {
                 x : 0,
@@ -38,7 +42,7 @@ export default class Main extends React.Component {
 
     handleAddRectangle = () => {
         this.setState({
-            rectangles: this.state.rectangles.concat([{length: 0, height: 0}])
+            rectangles: this.state.rectangles.concat([{length: "", height: ""}])
         });
     };
 
@@ -58,22 +62,26 @@ export default class Main extends React.Component {
 
     renderRectangleInput() {
         return <div>
-            <h4>Rectangles</h4>
-            <p>Length, height, id:</p>
+            <style>{'body { background-color: white; }'}</style> {/* background color */}
+            <div class="text-monospace p-3 mb-2 bg-warning text-dark text-center font-weight-bolder">Rectangle(s) Inputs </div>
+            <div class="text-monospace p-3 mb-2 bg-warning text-dark text-center font-weight-bolder">Length, Height, ID:</div>
             {this.state.rectangles.map((rectangle, idx) => (
                 <div className="rectangle">
-                    <input
+                    <box><input
                         type="number"
+                        min = "0"
                         value={rectangle.length}
                         name={"length"}
                         onChange={this.handleRectangleNameChange(idx)}
-                    />
+                    /> </box>
                     <input
                         type="number"
+                        min = "0"
                         value={rectangle.height}
                         name={"height"}
                         onChange={this.handleRectangleNameChange(idx)}
                     />
+                    
                     <button
                         type="button"
                         onClick={this.handleRemoveRectangle(idx)}
@@ -81,10 +89,14 @@ export default class Main extends React.Component {
                     >
                         -
                     </button>
+                    
                 </div>
+                
             ))}
         </div>
+        
     }
+    
 
     handleChangeAbstract = (event) => {
         this.setState({[event.target.name]: event.target.value});
@@ -97,24 +109,27 @@ export default class Main extends React.Component {
             <div>
                 <Info values={this.state.values} />
                 <form onSubmit={this.handleSubmit}>
-                    <label>
-                        Area to fill length:
-                        <input type="text" name="area_length" onChange={this.handleChangeAbstract}/>
-                        Area to fill height:
-                        <input type="text" name="area_height" onChange={this.handleChangeAbstract}/>
-                    </label>
+                <div class="text-monospace p-3 mb-2 bg-warning text-dark text-center font-weight-bolder">Material Site Application</div>
+                <div class="text-monospace p-3 mb-2 bg-warning text-dark text-center font-weight-bolder">
+                        Area to fill length: 
+                        <input type="text" min="0" name="area_length" onChange={this.handleChangeAbstract}/>
+                        </div>
+                        <div class="text-monospace p-3 mb-2 bg-warning text-dark text-center font-weight-bolder">
+                        Area to fill height: 
+                        <input type="text" min="0" name="area_height" onChange={this.handleChangeAbstract}/>
+                    </div>
                     {this.renderRectangleInput()}
                     <button
                         type="button"
                         onClick={this.handleAddRectangle}
                         className="small"
-                    >
-                        Add Rectangle
+                    >Add Rectangle
                     </button>
-                    <button>Incorporate</button>
+                    <button className="small">Insert Rectangle</button>
                 </form>
                 <Konva_Wrapper points={this.state.points} area_length={this.state.area_length} area_height={this.state.area_height} handleClick={this.handleClick.bind(this)}/>
             </div>
+            
         );
 
     }
